015e94c4f01652739ced85bcd12aaa244fa13a72
mhsnmk <mhsnmk3@gmail.com>
2024-03-17 20:54:44 +0500

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,7 +1,7 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "default", mintText: "Min Your NFT!", buttonLabel: "Mint now!", header: "Free Mint"}
+  { label: "default", mintText: "Mint Your NFT!", buttonLabel: "Mint now!", header: "Free Mint"}
 ];
 
 //header image in the ui. replace with your own
